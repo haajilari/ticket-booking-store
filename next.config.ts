be46479464c603fb1542a2b51c754a2444cbc242
unknown <amirhossein.hajilari@gmail.com>
2025-06-04 02:43:30 +0330

    @haajilari: next export check for cloudflare deploy

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
 };
 // Import next-pwa
+const isExport = process.env.NEXT_EXPORT === "true";
+
 const withPWA = require("next-pwa")({
   dest: "public", // Path to store Service Worker and related files (usually public)
   register: true, // Should the Service Worker be automatically registered on the client?
   skipWaiting: true, // Should a new Service Worker activate immediately (instead of waiting for all old tabs to close)?
-  disable: false, // Disable PWA in development mode (optional, but useful to avoid caching issues during development)
+  disable: isExport, // Disable PWA if static export is running
   // runtimeCaching: [...] // You can define caching strategies for runtime here (advanced)
 });
 // export default nextConfig;
