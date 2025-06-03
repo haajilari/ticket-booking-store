2a8b12e8dc68de13e1ecae3e2caec3aebb39b4b0
unknown <amirhossein.hajilari@gmail.com>
2025-06-04 02:46:36 +0330

    @haajilari: cross-env added to project, build command changed

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isExport = process.env.npm_lifecycle_event === "build";
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: isExport,
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
   reactStrictMode: true,
   output: "export",
 };
-// Import next-pwa
-const isExport = process.env.NEXT_EXPORT === "true";
 
-const withPWA = require("next-pwa")({
-  dest: "public", // Path to store Service Worker and related files (usually public)
-  register: true, // Should the Service Worker be automatically registered on the client?
-  skipWaiting: true, // Should a new Service Worker activate immediately (instead of waiting for all old tabs to close)?
-  disable: isExport, // Disable PWA if static export is running
-  // runtimeCaching: [...] // You can define caching strategies for runtime here (advanced)
-});
-// export default nextConfig;
 module.exports = withPWA(nextConfig);
